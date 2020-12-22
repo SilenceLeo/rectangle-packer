e2faee492ed5d295f4114da5bfdc545b578b75da
Leo <ly.liyang521@gmail.com>
2020-12-23 00:02:37 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { rectanglePacker, rectanglePackerMutation, Rectangle, RectangleSize } from "./rectanglePacker";
 export { rectanglePacker as default } from "./rectanglePacker";
 export { default as MaxRectsBinPack } from "./MaxRectsBinPack";
+export { default as GuillotineBinPack } from "./GuillotineBinPack";
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const rectanglePacker = require("./").rectanglePackerMutation;
 const MaxRectsBinPack = require("./").MaxRectsBinPack;
+const GuillotineBinPack = require("./").GuillotineBinPack;
 const arr = [
     [128, 317],
     [61, 61],
@@ -38,35 +39,54 @@ const arr = [
 //     [604, 314],
 //     [3, 3],
 // ];
-// const obj = arr.map(([width, height]) => ({ width, height, TE: 1 }));
+const obj = arr.map(([width, height]) => ({ width, height, TE: 1 }));
 // console.log(rectanglePacker);
-// const packObj = rectanglePacker(obj);
+const packObj = rectanglePacker(obj);
 // console.log(obj);
-// console.log(packObj);
-// packObj.forEach((p) => {
-//     console.log(p.TE);
-//     console.log(p.x);
-// });
+console.log(packObj);
 
-const pack = new MaxRectsBinPack(2048, 2048);
-const rectangles = arr.sort((a, b) => a.height - b.height).map(([width, height]) => ({ width, height }));
-// [
-//     {
-//         width: 20,
-//         height: 100,
-//         id: "1",
-//     },
-//     {
-//         width: 200,
-//         height: 70,
-//         id: "2",
-//     },
-//     {
-//         width: 30,
-//         height: 70,
-//         id: "3",
-//     },
-// ];
+let width = 0;
+let height = 0;
 
-const result = pack.InsertRects(rectangles, MaxRectsBinPack.FreeRectChoiceHeuristic.RectBestShortSideFit);
-console.log("result", pack, result);
+packObj.forEach((p) => {
+    width = Math.max(width, p.x + p.width);
+    height = Math.max(height, p.y + p.height);
+});
+
+// const pack = new MaxRectsBinPack(2048, 2048);
+const rectangles = arr.sort((a, b) => a.height - b.height).map(([width, height], index) => ({ width, height, index }));
+// // [
+// //     {
+// //         width: 20,
+// //         height: 100,
+// //         id: "1",
+// //     },
+// //     {
+// //         width: 200,
+// //         height: 70,
+// //         id: "2",
+// //     },
+// //     {
+// //         width: 30,
+// //         height: 70,
+// //         id: "3",
+// //     },
+// // ];
+
+// const result = pack.InsertRects(rectangles, MaxRectsBinPack.FreeRectChoiceHeuristic.RectBestShortSideFit);
+// console.log("result", pack, result);
+const pack = new GuillotineBinPack(810, 600);
+
+console.log(pack.InsertSizes(rectangles, true, 1, 1));
+
+console.log(pack.usedRectangles);
+
+let w = 0;
+let h = 0;
+
+pack.usedRectangles.forEach((p) => {
+    w = Math.max(w, p.x + p.width);
+    h = Math.max(h, p.y + p.height);
+});
+console.log(width, height, width * height);
+console.log(w, h, w * h);
